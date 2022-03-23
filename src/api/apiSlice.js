2251f1978a72eb711f4e545dfb458e49685c9379
Valentin <veksel_or_vels@mail.ru>
2022-03-23 22:25:55 +0300

    make it better

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -1,8 +1,41 @@
+// import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
+// export const apiSlice = createApi({
+//   reducerPath: 'api',
+//   baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:3001'}),
+//   tagTypes: ['Heroes'],
+//   endpoints: builder => ({
+//     getHeroes: builder.query({
+//       query: () => 'heroes',
+//       providesTags: ['Heroes']
+//     }),
+//     createHero: builder.mutation({
+//       query: hero => ({
+//         url: '/heroes',
+//         method: 'POST',
+//         body: hero
+//       }),
+//       invalidatesTags: ['Heroes']
+//     }),
+//     deleteHero: builder.mutation({
+//       query: id => ({
+//         url: `/heroes/${id}`,
+//         method: 'DELETE'
+//       }),
+//       invalidatesTags: ['Heroes']
+//     })
+//   })
+// });
+
+// //Экспорт хука
+// export const {useGetHeroesQuery, useCreateHeroMutation, useDeleteHeroMutation} = apiSlice; 
+
+
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:3001'}),
+  baseQuery: fetchBaseQuery({baseUrl: 'https://heroes-fake-api.herokuapp.com'}),
   tagTypes: ['Heroes'],
   endpoints: builder => ({
     getHeroes: builder.query({
@@ -29,3 +62,5 @@ export const apiSlice = createApi({
 
 //Экспорт хука
 export const {useGetHeroesQuery, useCreateHeroMutation, useDeleteHeroMutation} = apiSlice; 
+
+
