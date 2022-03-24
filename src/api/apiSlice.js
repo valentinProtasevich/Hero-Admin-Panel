1c4dac591dcb7c51161e0d9af0a6b6063c68596f
Valentin <veksel_or_vels@mail.ru>
2022-03-24 19:02:19 +0300

    Configure React app for deployment to GitHub Pages

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -1,36 +1,3 @@
-// import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
-
-// export const apiSlice = createApi({
-//   reducerPath: 'api',
-//   baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:3001'}),
-//   tagTypes: ['Heroes'],
-//   endpoints: builder => ({
-//     getHeroes: builder.query({
-//       query: () => 'heroes',
-//       providesTags: ['Heroes']
-//     }),
-//     createHero: builder.mutation({
-//       query: hero => ({
-//         url: '/heroes',
-//         method: 'POST',
-//         body: hero
-//       }),
-//       invalidatesTags: ['Heroes']
-//     }),
-//     deleteHero: builder.mutation({
-//       query: id => ({
-//         url: `/heroes/${id}`,
-//         method: 'DELETE'
-//       }),
-//       invalidatesTags: ['Heroes']
-//     })
-//   })
-// });
-
-// //Экспорт хука
-// export const {useGetHeroesQuery, useCreateHeroMutation, useDeleteHeroMutation} = apiSlice; 
-
-
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
